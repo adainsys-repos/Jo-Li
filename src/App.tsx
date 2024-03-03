602c36aa3499b01a6fd22f55669908fa84dbbb3a
Chandra Bose <chandrabosep3112@gmail.com>
2024-03-03 14:34:58 +0530

    feat: jobs dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,68 @@
-// import { Edit, Plus } from "lucide-react";
+//@ts-nocheck
+import { Button } from "./components/ui/button";
 
-import ChangeUserPswd from "./components/ChangeUserPswd";
-import CreateUser from "./components/CreateUser";
-
-// import {
-//   Sheet,
-//   SheetContent,
-//   SheetHeader,
-//   SheetTitle,
-//   SheetTrigger,
-// } from "@/components/ui/sheet";
-
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
-// import { Input } from "./components/ui/input";
-// import { Button } from "./components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useEffect, useState } from "react";
+import axiosInstance from "../config/axiosInstance";
 
 export default function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const data = await axiosInstance.get("/jobs/all-jobIds", {
+        headers: {
+          Authorization: `Bearer ${
+            document.cookie
+              .split(";")
+              .find((c) => c.trim().startsWith(`token=`))
+              ?.split("=")[1] || null
+          } `,
+        },
+      });
+
+      setJobs(data.data);
+    };
+
+    fetchJobs();
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="w-10/12 h-full m-auto flex flex-col py-10 ">
-      {/* <div className="w-full flex flex-col gap-4">
-        {[
-          { name: "frontend", id: 1020 },
-          { name: "backend", id: 1021 },
-        ].map((item) => (
-          <div className="flex items-center gap-4 space-x-4 w-full">
-            <div className=" bg-[#1C1C1C] w-8/12 px-4 py-2 rounded-md">
-              <p className="text-white/90 capitalize text-xl font-medium">
-                {item.name}
-              </p>
-              <p className="text-white/90">{item.id}</p>
-            </div>
-            <div className="flex gap-4">
-              <div className="flex items-center justify-center">
-                <Edit className="w-5 h-5 text-white/50" strokeWidth={1.5} />
-              </div>
-              <div className="flex items-center justify-center w-8 h-8 bg-blue-500 rounded-full">
-                <Sheet>
-                  <SheetTrigger>
-                    <Plus className="w-6 h-6 text-white" />
-                  </SheetTrigger>
-                  <SheetContent className="bg-[#100c0c]">
-                    <SheetHeader>
-                      <SheetTitle className="text-white">Edit Job</SheetTitle>
-                    </SheetHeader>
-                    <div className="py-6 space-y-4">
-                      <Select>
-                        <SelectTrigger className="w-10/12 bg-[#1C1C1C] border-0 text-white/60">
-                          <SelectValue placeholder="Job" />
-                        </SelectTrigger>
-                        <SelectContent className="bg-[#1C1C1C] border-0 text-white/60">
-                          {[
-                            { platform: "Linkedin", value: "linkedin" },
-                            { platform: "Linkedin", value: "linkedin" },
-                          ].map((e) => (
-                            <SelectItem value={e.value}>
-                              {e.platform}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <Input
-                        placeholder="TB"
-                        className="bg-[#1C1C1C] border-0 text-white/90"
-                      />
-                      <div className="flex gap-2">
-                        <Button>Save</Button>
-                        <Button>Cancel</Button>
-                      </div>
-                    </div>
-                  </SheetContent>
-                </Sheet>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div> */}
-      <div className="flex flex-col w-1/3 gap-4">
-        <ChangeUserPswd />
-        <CreateUser />
+      <div className="flex justify-between items-center">
+        <h4 className="text-white/90 text-2xl font-medium">Main Dashboard</h4>
+        <Button className="bg-blue-500 px-8">Add</Button>
+      </div>
+      <div className="text-white/90 py-10">
+        <Accordion type="single" className="space-y-4" collapsible>
+          {jobs &&
+            jobs.map((e) => (
+              <AccordionItem
+                value={e}
+                className="border border-[#1C1C1C] px-8 rounded-lg"
+              >
+                <AccordionTrigger className="flex w-full justify-end gap-8 hover:no-underline">
+                  <div className="flex w-full gap-20">
+                    <p>{e.JobSource.name}</p>
+                    <p>{e.jobId}</p>
+                  </div>
+                  <Button className="bg-[#1C1C1C] hover:bg-[#1C1C1C]/60 px-8">
+                    View
+                  </Button>
+                </AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+        </Accordion>
       </div>
     </div>
   );
